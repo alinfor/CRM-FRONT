adc54f73c8f2723918214e44856d0211d2d4b6cc
Alifor <116239087+alinfor@users.noreply.github.com>
2023-12-21 09:09:41 +0100

    footer

---
+++ src/page/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <footer className="bg-gray-800 text-white p-4 mt-8">
+    <div className="container mx-auto">
+      <p className="text-center">© 2023 Mon Application. Tous droits réservés.</p>
+    </div>
+  </footer>
+  )
+}
