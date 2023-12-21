2bd4fb43962b43f5b0f7fe94161d28b9f618ed0c
Alifor <116239087+alinfor@users.noreply.github.com>
2023-12-21 09:09:49 +0100

    header

---
+++ src/page/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+  <header className="bg-gray-800 text-white p-4">
+      <div className="container mx-auto">
+        <h1 className="text-2xl font-semibold">Mon Application</h1>
+      </div>
+    </header>
+  )
+}
